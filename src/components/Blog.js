0e960c24d9b3c82e1c30650511a70dd3fb6da73d
Marcelo Brito <marcellus.brito@gmail.com>
2023-08-16 23:26:32 -0300

    props validation, footer and sidebar

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,12 +1,14 @@
 import React from "react"
-//import PropTypes from "prop-types"
+import PropTypes from "prop-types"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { Link } from "gatsby"
 const Blog = ({ id, title, image, date, category, slug, desc }) => {
   return (
     <Link to={`/blogs/${slug}`} key={id} className="blog">
       <article>
-        <GatsbyImage image={getImage(image.localFile)} className="blog-img" />
+        {image && (
+          <GatsbyImage image={getImage(image.localFile)} className="blog-img" />
+        )}
         <div className="blog-card">
           <h4>{title}</h4>
           <p>{desc}</p>
@@ -20,6 +22,14 @@ const Blog = ({ id, title, image, date, category, slug, desc }) => {
   )
 }
 
-Blog.propTypes = {}
+Blog.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+  slug: PropTypes.string.isRequired,
+  image: PropTypes.object.isRequired,
+}
 
 export default Blog
