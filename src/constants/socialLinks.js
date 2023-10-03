d4a420e5df40720d504cdb0e6475b0135ba67996
Marcelo Brito <marcellus.brito@gmail.com>
2023-10-03 14:14:19 -0300

    soacial link update

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -10,7 +10,7 @@ const data = [
   {
     id: 2,
     icon: <FaLinkedin className="social-icon"></FaLinkedin>,
-    url: "https://www.linkedin.com/in/marcelo-brito-23096733/",
+    url: "https://www.linkedin.com/in/marcelosbrito/",
   },
   {
     id: 3,
