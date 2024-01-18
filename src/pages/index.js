ef6556520e253d798a342b4c70b64f0bcd8450e5
Marcelo Brito <marcellus.brito@gmail.com>
2024-01-18 17:26:59 -0300

    New asset, new project sorting

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ export const Head = () => (
 
 export const query = graphql`
   query {
-    allStrapiProject(filter: { feature: { eq: true } }) {
+    allStrapiProject(sort: { title: ASC }, filter: { feature: { eq: true } }) {
       nodes {
         id
         description
